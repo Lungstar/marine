ba0b83ceb089da6af80e6df77f8f2dde81de2a28
lungaM <lnkosi@retrorabbit.co.za>
2020-08-27 15:45:51 +0200

    created generic screen holder

---
+++ marine_life/lib/components/screen_holder.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ScreenHolder extends StatefulWidget {
+  final Widget body;
+  final bool showDrawer;
+  final bool loggedInUser;
+  final Color backgroundColor;
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const ScreenHolder(
+      {Key key,
+        @required this.body,
+        this.scaffoldKey,
+        this.backgroundColor: Colors.white,
+        this.loggedInUser: true,
+        this.showDrawer: true})
+      : super(key: key);
+
+  @override
+  ScreenHolderState createState() => ScreenHolderState();
+}
+
+class ScreenHolderState extends State<ScreenHolder> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+      return Scaffold(
+        backgroundColor: widget.backgroundColor,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+        ),
+        body: widget.body,
+      );
+    }
+
+}
